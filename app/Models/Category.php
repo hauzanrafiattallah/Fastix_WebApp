8492a54723d1612333b062fe3d58f635d8c7c12c
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-02-26 21:00:19 +0700

    FAST-2 | feat: add relationships for BookingTransaction, Category, and Seller models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
@@ -17,5 +18,8 @@ class Category extends Model
         'icon',
     ];
 
-
+    public function tickets(): HasMany
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Seller extends Model
@@ -18,4 +19,9 @@ class Seller extends Model
         'slug',
         'photo',
     ];
+
+    public function tickets(): HasMany 
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
